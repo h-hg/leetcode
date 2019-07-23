e44df0c35c7ec5fc38828c5240bd0fd98a8dca26
h-hg <1394564544@qq.com>
2019-07-23 23:13:58 +0800

    2019-7-23

---
+++ 69. Sqrt(x)/solution.cpp
@@ -0,0 +1,24 @@
+//binary search
+class Solution {
+public:
+    int mySqrt( int x ) {
+        if( x < 4 )// 0 1 2 3
+            return x;
+        long low = 1, high = x, mid, product; // 'long' avoids overflow
+        while( low <= high ) {
+            mid = low + (high - low) / 2;
+            product = mid*mid;
+            if( product == x )
+                return mid;
+            if( product < x )
+                low = mid + 1;
+            else
+                high = mid - 1 ;
+        }
+        // because we could have incremented mid when 'product < x'
+        if( mid * mid > x ) mid = mid - 1;
+        return mid;
+    }
+};
+
+//Newton's method (Newton-Raphson method)
